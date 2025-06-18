e00f7810ca6f23e55b3f1f3408b82206a5a25dba
maton369 <ysk901085@gmail.com>
2025-06-18 11:14:40 +0900

    Add: State Machineの実装

--- go.mod
+++ go.mod
@@ -3,15 +3,18 @@ module raft-redis-cluster
 go 1.24.2
 
 require (
+	github.com/bootjp/go-kvlib v0.0.0-20250516142503-84105e3f810c
+	github.com/hashicorp/raft v1.7.3
+)
+
+require (
 	github.com/armon/go-metrics v0.4.1 // indirect
-	github.com/bootjp/go-kvlib v0.0.0-20250516142503-84105e3f810c // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/hashicorp/go-hclog v1.6.2 // indirect
 	github.com/hashicorp/go-immutable-radix v1.0.0 // indirect
 	github.com/hashicorp/go-metrics v0.5.4 // indirect
 	github.com/hashicorp/go-msgpack/v2 v2.1.2 // indirect
 	github.com/hashicorp/golang-lru v0.5.0 // indirect
-	github.com/hashicorp/raft v1.7.3 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/spaolacci/murmur3 v1.1.0 // indirect
---
+++ raft/state_machine.go
@@ -0,0 +1,75 @@
+package raft
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"io"
+	"raft-redis-cluster/store"
+
+	"github.com/hashicorp/raft"
+)
+
+var _ raft.FSM = (*StateMachine)(nil)
+
+type Op int
+
+const (
+	Put Op = iota
+	Del
+)
+
+type KVCmd struct {
+	Op  Op     `json:"op"`
+	Key []byte `json:"key"`
+	Val []byte `json:"val"`
+}
+
+func NewStateMachine(store store.Store) *StateMachine {
+	return &StateMachine{store}
+}
+
+type StateMachine struct {
+	store store.Store
+}
+
+// Apply applies a Raft log entry to the key-value store.
+func (s *StateMachine) Apply(log *raft.Log) any {
+	ctx := context.Background()
+	c := KVCmd{}
+
+	err := json.Unmarshal(log.Data, &c)
+	if err != nil {
+		return err
+	}
+
+	return s.handleRequest(ctx, c)
+}
+
+// Restore stores the key-value store to a previous state.
+func (s *StateMachine) Restore(rc io.ReadCloser) error {
+	return s.store.Restore(rc)
+}
+
+// Snapshot returns a KVSnapshot of the key-value store.
+func (s *StateMachine) Snapshot() (raft.FSMSnapshot, error) {
+	rc, err := s.store.Snapshot()
+	if err != nil {
+		return nil, err
+	}
+
+	return &KVSnapshot{rc}, nil
+}
+
+var ErrUnknownOp = errors.New("unknown op")
+
+func (s *StateMachine) handleRequest(ctx context.Context, cmd KVCmd) error {
+	switch cmd.Op {
+	case Put:
+		return s.store.Put(ctx, cmd.Key, cmd.Val)
+	case Del:
+		return s.store.Delete(ctx, cmd.Key)
+	default:
+		return ErrUnknownOp
+	}
+}
